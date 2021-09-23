df1725a8a3cc29e59958e06e3f2e7bbd816996f6
husky339 <bendermaj@gmail.com>
2021-09-23 15:03:31 -0400

    Trying to get player to rotate towards mouse for some reason it thinks the mouse is always in the middle of the screen

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    Rigidbody rb;
+    Rigidbody2D rb;
     [SerializeField] float speed;
     [SerializeField] float rotSpeed;
+    [SerializeField] private Camera cam;
 
     private float goUp;
     private float goRight;
 
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {    }
+    {
+        //Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg) - 90f;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+
+
+
+    }
     private void FixedUpdate()
     {
-        BasicMove();   
+        BasicMove();
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
     private void BasicMove()
@@ -30,6 +46,6 @@ public class BasicMovement : MonoBehaviour
         //Using getaxis for smoothing movement
         goUp = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         goRight = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        rb.velocity = new Vector3(goRight, 0, goUp);
+        rb.velocity = new Vector3(goRight, goUp, 0);
     }
 }
